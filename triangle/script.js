c0f77c7e92333442dddb6083fafd6fd022983d73
garunkumar27 <garunk.27@gmail.com>
2022-11-03 16:54:28 +0530

    Star Pattern

--- triangle/script.js
+++ triangle/script.js
@@ -1,18 +1,20 @@
 // Left triangle pattern 
-// let lft=document.querySelector("#left").innerHTML="Left Triangle Pattern";
-// let numb=5,val="";
-// for(i=1;i<=numb;i++){
-//     for(j=0;j<i;j++){
-//         val+="*";
-//     }
-//     val+="<br>";
-//     // val+="\n";
-// }
-// // console.log(val);
-// document.write(val);
+let lft=document.querySelector("#left").innerHTML="Left Triangle Pattern";
+let lftCnt=document.querySelector("#left_star");
+let numb=5,val="";
+for(i=1;i<=numb;i++){
+    for(j=0;j<i;j++){
+        val+="*";
+    }
+    val+="<br>";
+    // val+="\n";
+}
+lftCnt.innerHTML=val;
+// console.log(val);
 
 
 let rt=document.querySelector("#right").innerHTML="Right Triangle Pattern";
+let rtCnt=document.querySelector("#right_star");
 let num=5,str="";
 for(i=1;i<=num;i++){
     // Printing Spaces
@@ -25,10 +27,14 @@ for(i=1;i<=num;i++){
     str+="<br>";
     // str+="\n"
 }
+rtCnt.innerHTML=str
 // console.log(str);
-document.write(str);
+// document.write(str);
 
 // Downward Triangle Pattern
+let dnCnt=document.querySelector("#down_star"), 
+dn=document.querySelector("#down").innerHTML="Downward Triangle Pattern";
+
 let n=5,vals="";
 for(i=0;i<n;i++){
     //Printing Star
@@ -38,6 +44,30 @@ for(i=0;i<n;i++){
     // vals+="\n"
     vals+="<br>";
 }
-// document.write(vals);
+dnCnt.innerHTML=vals;
+// console.log(vals);
 
-// 
+//Hallow Triangle Pattern
+let nu=6;hallow="";
+for(i=1;i<=nu;i++){
+	//Printing Star
+	for(j=0;j<i;j++){
+		if(i==nu){
+			hallow+="*";
+		}
+		else{
+			if(j==0||j==i-1){
+				hallow+="*";
+			}	
+			    // Printing Spaces
+			else{
+				hallow+=" ";
+				}
+	}
+	}
+	hallow+="\n";
+	//hallow+="<br>"
+	
+}
+document.write(hallow);
+console.log(hallow);
